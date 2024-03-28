84d16505dd03a9a1cc8a0c7b2c6f89ada24b9ef3
Ghulam Akbar <ghulamakbarabbbro110@gmail.com>
2024-03-28 09:14:51 +0500

    first commit

---
+++ index.ts
@@ -0,0 +1,46 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+  { message: "Enter first number", type: "number", name: "Firstnumber" },
+  { message: "Enter second number", type: "number", name: "Secondnumber" },
+  {
+    message: "Select operator to perform act",
+    type: "list",
+    name: "operator",
+    choices: [
+      "Addition (+)",
+      "Subtraction (-)",
+      "Multiplication (x)",
+      "Division (/)",
+    ],
+  },
+]);
+console.log(answer);
+
+if (answer.operator === "Addition (+)") {
+  console.log(
+    `Your answer of ${answer.Firstnumber} + ${answer.Secondnumber} is : ${
+      answer.Firstnumber + answer.Secondnumber
+    }`
+  );
+} else if (answer.operator === "Subtraction (-)") {
+  console.log(
+    `Your answer of ${answer.Firstnumber} - ${answer.Secondnumber} is : ${
+      answer.Firstnumber - answer.Secondnumber
+    }`
+  );
+} else if (answer.operator === "Multiplication (x)") {
+  console.log(
+    `Your answer of ${answer.Firstnumber} X ${answer.Secondnumber} is : ${
+      answer.Firstnumber * answer.Secondnumber
+    }`
+  );
+} else if (answer.operator === "Division (/)") {
+  console.log(
+    `Your answer of ${answer.Firstnumber} / ${answer.Secondnumber} is : ${
+      answer.Firstnumber / answer.Secondnumber
+    }`
+  );
+} else {
+  console.log("Kindly select the operator");
+}
